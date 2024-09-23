720aef2cdd93dba91e72aed4a73932f4eaa0981e
Anurag-Kumar9 <kumar94anurag@gmail.com>
2024-09-23 23:18:03 +0530

    problem 1 of lab7

---
+++ lab7_q1.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class SegmentTree {
+    vector<int> tree;
+    int n;
+
+    void build(const vector<int>& arr, int node, int start, int end) {
+        if (start == end) {
+            tree[node] = arr[start];
+        } else {
+            int mid = (start + end) / 2;
+            build(arr, 2 * node, start, mid);
+            build(arr, 2 * node + 1, mid + 1, end);
+            tree[node] = tree[2 * node] + tree[2 * node + 1];
+        }
+    }
+
+    void update(int node, int start, int end, int idx, int val) {
+        if (start == end) {
+            tree[node] = val;
+        } else {
+            int mid = (start + end) / 2;
+            if (start <= idx && idx <= mid) {
+                update(2 * node, start, mid, idx, val);
+            } else {
+                update(2 * node + 1, mid + 1, end, idx, val);
+            }
+            tree[node] = tree[2 * node] + tree[2 * node + 1];
+        }
+    }
+
+    int query(int node, int start, int end, int L, int R) {
+        if (R < start || end < L) {
+            return 0;
+        }
+        if (L <= start && end <= R) {
+            return tree[node];
+        }
+        int mid = (start + end) / 2;
+        int left_sum = query(2 * node, start, mid, L, R);
+        int right_sum = query(2 * node + 1, mid + 1, end, L, R);
+        return left_sum + right_sum;
+    }
+
+public:
+    SegmentTree(const vector<int>& arr) {
+        n = arr.size();
+        tree.resize(4 * n);
+        build(arr, 1, 0, n - 1);
+    }
+
+    void update(int idx, int val) {
+        update(1, 0, n - 1, idx, val);
+    }
+
+    int query(int L, int R) {
+        return query(1, 0, n - 1, L, R);
+    }
+};
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> arr(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> arr[i];
+    }
+    SegmentTree segTree(arr);
+    while (Q--) {
+        int type;
+        cin >> type;
+        if (type == 1) {
+            int idx, val;
+            cin >> idx >> val;
+            segTree.update(idx, val);
+        } else if (type == 2) {
+            int L, R;
+            cin >> L >> R;
+            cout << segTree.query(L, R) << endl;
+        }
+    }
+    return 0;
+}
